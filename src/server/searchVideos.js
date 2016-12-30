189e7fc3b81a350c0c9cbb02ee41193bedc588ea
Stainless Pot <deian.goranov@gmail.com>
2016-12-30 14:29:17 +0200

    Fixed searchVideos only to fetch videos

--- src/server/searchVideos.js
+++ src/server/searchVideos.js
@@ -9,7 +9,7 @@ export default function (request, response, next){
 
   let apiKey = 'AIzaSyBaw0mOT7PZm5RrHr-s35yXOY6fzoCBdsY';
 
-  axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&key=${apiKey}&q=${query.q}`)
+  axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&key=${apiKey}&q=${query.q}`)
   .then(res => {
     response.end(JSON.stringify(res.data));
     next();
